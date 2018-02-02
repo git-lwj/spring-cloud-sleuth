f6e7242a17805b997baabd6b7d48a9993cd2fcc2
梁文剑 <liangwj9@ziroom.com>
2018-02-02 16:01:34 +0800

    update

--- eureka-server/src/main/java/com/lwj/study/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/lwj/study/eureka/EurekaServerApplication.java
@@ -2,6 +2,7 @@ package com.lwj.study.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -22,6 +23,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class,args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
